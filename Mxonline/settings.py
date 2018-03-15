0f934d084486055c833f6fe03d9e1d66652fe33b
yanghl <yanghl@ngarihealth.com>
2018-03-15 14:07:04 +0800

    项目配置

--- Mxonline/settings.py
+++ Mxonline/settings.py
@@ -13,18 +13,23 @@ import sys
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-sys.path.insert(0,os.path.join(BASE_DIR, 'extra_apps'))
+# os.path.abspath:获取当前文件路径
+# os.path.dirname：获取当前文件的上级文件夹路径
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 获取项目基础路径
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))  # 将apps模块添加到python搜索路径（Path环境变量）的第一个
+sys.path.insert(0,os.path.join(BASE_DIR, 'extra_apps'))  # 将extra_apps模块添加到python搜索路径（Path环境变量）的第一个
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+1nruftzf9)mi3mb!0_wpsmw=pne(717j(xszq_ghmap9n03sw'
 
+# 开启调试
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# 允许你设置哪些域名可以访问，即使在 Apache 或 Nginx 等中绑定了，这里不允许的话，也是不能访问的。
+# 当 DEBUG=False 时，这个为必填项，如果不想输入，可以用 ALLOW_HOSTS = ['*'] 来允许所有的。
 ALLOWED_HOSTS = ['*']
 
 
@@ -32,10 +37,9 @@ ALLOWED_HOSTS = ['*']
 # 设置邮箱和用户名均可登录
 AUTHENTICATION_BACKENDS = (
     'users.views.CustomBackend',
-
 )
 
-# App注册
+# 应用注册列表
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -71,7 +75,7 @@ ROOT_URLCONF = 'Mxonline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 模板索引目录
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,17 +143,19 @@ USE_L10N = True
 USE_TZ = False
 
 
+# static 是静态文件所有目录
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
+# 一般来说我们只要把静态文件放在 APP 中的 static 目录下，
+# 部署时用 python manage.py collectstatic 就可以把静态文件收集到（复制到）STATIC_ROOT 目录，
+# 但是有时我们有一些共用的静态文件，这时候可以设置 STATICFILES_DIRS 另外弄一个文件夹，如下：
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
 # 发送邮件的setting设置
-
 EMAIL_HOST = "smtp.qq.com"
 EMAIL_PORT = 25
 EMAIL_HOST_USER = "mxonline@yanghl.cn"
@@ -158,7 +164,6 @@ EMAIL_USE_TLS= True
 EMAIL_FROM = "mxonline@yanghl.cn"
 
 # 设置我们上传文件的路径
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
